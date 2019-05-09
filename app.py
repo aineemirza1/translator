48ff694b4f5434ad62976a652989fc621d3d990a
aineemirza1 <aineeijaz2001@gmail.com>
2019-05-09 11:30:46 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from flask import Flask, render_template, request
+from flask_json import FlaskJSON, json_response
+
+app = Flask(__name__)
+FlaskJSON(app)
+STATUS_OK = "ok"
+STATUS_ERROR = "error"
+
+
+@app.route('/')
+def gui():
+    return render_template('app.html')
+
+
+@app.route('/index', methods=['POST'])
+def index():
+    return json_response(status="success", data="Hello Word")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
